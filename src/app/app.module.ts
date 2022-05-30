b6f9894df0ace85551aacbbf5ea58d6857b663fb
devofthings <chris-winter@live.de>
2022-05-30 15:22:05 +0200

    provide factory via app instead service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { AppComponent } from "./app.component";
+import { FlightSearchComponent } from "./flight-search/flight-search.component";
+import { createFlightService } from "./factories/flight-service.factory";
+import { FlightService } from "./services/flight.service";
 
 @NgModule({
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  declarations: [AppComponent, SidebarComponent, NavbarComponent, FlightSearchComponent],
-  providers: [],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FlightSearchComponent,
+  ],
+  providers: [
+    {
+      provide: FlightService,
+      useFactory: createFlightService,
+      deps: [HttpClient],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -1,14 +1,8 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { createFlightService } from "../factories/flight-service.factory";
 import { Flight } from "../model/flight";
 
-@Injectable({
-  providedIn: "root",
-  useFactory: createFlightService,
-  deps: [HttpClient],
-})
+@Injectable()
 export abstract class FlightService {
   abstract search(from: string, to: string): Observable<Flight[]>;
 
